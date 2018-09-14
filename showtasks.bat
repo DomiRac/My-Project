e5f4fa30032a1ba70cb0e713f67c6c88351c1070
“DomiRac <dominika.rac@gmail.com>
2018-09-14 12:36:26 +0200

    17.6

--- showtasks.bat
+++ showtasks.bat
@@ -1,15 +1,15 @@
 call gradlew build
-if "%ERRORLEVEL%" == "0" goto rename
+if "%ERRORLEVEL%" == "0" goto openruncrud
 echo.
 echo GRADLEW BUILD has errors - breaking work
 goto fail
 
 :openruncrud
 call "C:\Users\domin\Desktop\tasks\runcrud.bat"
-if "%ERRORLEVEL%" == "0" goto fail
+if "%ERRORLEVEL%" == "0" goto rungooglechrome
 
 :rungooglechrome
-start chrome https://www.google.co.in/
+start https://www.google.com/
 goto end
 
 :fail
